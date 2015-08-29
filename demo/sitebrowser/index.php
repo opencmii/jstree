0e1fb95735dd5ff036bab22d3a3fe5783e2ebd0a
Phil <opencmii@users.noreply.github.com>
2015-08-28 23:58:19 -0400

    Added $port as part of the array processed by db::get()
    This extend the dbms parametrization by adding the database TCP port also.
    Useful, since I had to find a way to add it, my DEV MySQL dbms running on 33060...

--- demo/sitebrowser/index.php
+++ demo/sitebrowser/index.php
@@ -3,11 +3,11 @@ require_once(dirname(__FILE__) . '/class.db.php');
 require_once(dirname(__FILE__) . '/class.tree.php');
 $user    = "root";
 $pass    = "";
+$host     = "127.0.0.1";
 $schema  = "test";
-$url     = "127.0.0.1";
 $port    = "3306";
 if(isset($_GET['operation'])) {
-	$fs = new tree(db::get('mysqli://'.$user.':'.$pass.'@'.$host.'/'.$schema), array('structure_table' => 'tree_struct', 'data_table' => 'tree_data', 'data' => array('nm')));
+	$fs = new tree(db::get('mysqli://'.$user.':'.$pass.'@'.$host.'/'.$schema), array('port'=>$port, 'structure_table' => 'tree_struct', 'data_table' => 'tree_data', 'data' => array('nm')));
 	try {
 		$rslt = null;
 		switch($_GET['operation']) {
